2b320041459e922ebf89b3d2f45b0e79bd39d189
yahyaoui hamdi <hamdi.yahyaoui@oyez.fr>
2020-12-20 23:42:44 +0100

    create login function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Header = () => (
-  <div className="header">
-    <Link className="title">Todo List</Link>
-    <Link to="/">Home</Link>
-    <Link to="/taches">Taches</Link>
-  </div>
-);
+const Header = () => {
+  const logout = async () => {
+    await localStorage.removeItem("user");
+    await window.location.reload(true);
+  };
+  return (
+    <div className="header">
+      <Link className="title">Todo List</Link>
+
+      {localStorage.getItem("user") ? (
+        <div>
+          <Link to="/taches">Taches</Link>
+          <Link to="/" onClick={() => logout()}>
+            Déconnexion
+          </Link>
+        </div>
+      ) : (
+        <Link to="/">Home</Link>
+      )}
+    </div>
+  );
+};
 
 export default Header;
--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
-export const Login = () => (
-  <div className="login">
-      <form>
-          <label> Adresse e-mail</label>
+export const Login = () => {
+  const history = useHistory();
+  const successLogin = () => {
+    localStorage.setItem("user", JSON.stringify(dataUser));
+    history.push("/taches");
+    window.location.reload(true);
+  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dataUser = {
+    email: "test@test.com",
+    password: "test",
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    {
+      dataUser.email === email && dataUser.password === password
+        ? successLogin()
+        : console.log("Veuillez vérifier email et mot de passe");
+    }
+  };
+  return (
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <label> Adresse e-mail</label>
         <input
           name="email"
           type="text"
           placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <label> Mot de passe</label>
         <input
           name="password"
           type="password"
           placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <button className="btn" type="submit">Connexion</button>
+        <input className="btn" type="submit" value="Soumettre" />
       </form>
-  </div>
-);
+    </div>
+  );
+};
